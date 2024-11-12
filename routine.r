80717d3e8dccce4aede952fa130b1e55786fa5a5
charles-hebert <hebertcharles@hotmail.com>
2024-11-12 08:11:03 -0500

    Create routine.r
    avec les infos courriels ajoutées - je ne suis pas certain par contre que le fichier dont Melissa VanBussel est présent.

---
+++ routine.r
@@ -0,0 +1,38 @@
+#cbc
+#wikipedia
+#trends
+#envoi courriel
+
+
+
+library(blastula)
+
+email <- compose_email(
+  body = md("Bonjour,
+
+Voici les fichiers demandés en pièce jointe.
+  
+Pour toute info, n'hésitez pas à communiquer avec moi,
+Charles")
+)
+
+# Ajoutez les fichiers en pièce jointe (spécifiez le chemin de vos fichiers)
+fichiers_a_envoyer <- c("wikipedia/wikipedia.csv")
+for (fichier in fichiers_a_envoyer) {
+  email <- add_attachment(email, file = fichier)
+}
+
+# Envoyer l'email
+smtp <- smtp_send(
+  email,
+  from = "charles.hebert.osfi@outlook.com",
+  to = "charles.hebert@osfi-bsif.gc.ca",
+  subject = "Fichiers demandés",
+  credentials = creds_envvar(
+    user = "charles.hebert.osfi@outlook.com",
+    pass_envvar = "email_secret",
+    host = "smtp.exemple.com",
+    port = 465,
+    use_ssl = TRUE
+  )
+)
